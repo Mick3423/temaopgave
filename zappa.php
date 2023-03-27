dcbe8b4bc6cac2512f160e2e249534ba8fe3363b
mich1 <Mich.1999@hotmail.com>
2023-03-27 17:45:15 +0200

    billede + dropdown added

--- zappa.php
+++ zappa.php
@@ -29,5 +29,59 @@
 
 <!-- Her skal sidens indhold ligge -->
 
+
+
+<div class="container-sm">
+    <div><img src="images/zappa.jpg" class="img-fluid" alt="zappa"></div>
+    <h1>Zappa</h1>
+
+    <p>”Zappa” er en dansk ungdomsfilm fra 1983, instrueret af Bille August og baseret på et manuskript af forfatteren Bjarne Reuter. Filmen tager os med tilbage til 1960'erne og følger en gruppe unge drenge og deres dag til dag narrestreger, som langsomt udvikler sig til slemmere ting.  </p>
+
+    <a href="#" class=" btn bg-primary">læs mere her</a>
+    <h3 class="text-center">hvor spilles Zappa</h3>
+</div>
+
+<div class="accordion m-3" id="accordionExample">
+    <div class="accordion-item">
+        <h2 class="accordion-header">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Slagelse
+            </button>
+        </h2>
+        <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                8/4 2024
+            </div>
+        </div>
+    </div>
+    <div class="accordion-item">
+        <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Sorø
+            </button>
+        </h2>
+        <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              9/4 2024
+            </div>
+        </div>
+    </div>
+    <div class="accordion-item">
+        <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+               dianalund
+            </button>
+        </h2>
+        <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                10/4 2024
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
