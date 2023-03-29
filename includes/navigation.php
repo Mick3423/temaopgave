c9c93a2b19dccc1861f9289371bcc37d1d38b38c
mich1 <Mich.1999@hotmail.com>
2023-03-29 11:11:12 +0200

    logo som hjem knap + sider linket sammen i navigation og facebook og instagram tekst i footer

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,7 +1,7 @@
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary justify-content-end">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="index.php">
             <img src="images/vilmaLogo.png" alt="vilmalogo">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,15 +9,15 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Film
+                        Film/oplæg
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Zappa</a></li>
-                        <li><a class="dropdown-item" href="#">You and me forever</a></li>
+                        <li><a class="dropdown-item" href="zappa.php">Zappa</a></li>
+                        <li><a class="dropdown-item" href="youandme.php">You and me forever</a></li>
+                        <li><a class="dropdown-item" href="billeaugust.php">Bille August</a></li>
+                        <li><a class="dropdown-item" href="kasparmunk.php">Kaspar Munk</a></li>
 
                     </ul>
                 </li>
@@ -33,10 +33,10 @@
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">kontakt os</a>
+                    <a class="nav-link" href="kontakt.php">kontakt os</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">om os</a>
+                    <a class="nav-link" href="omos.php">om os</a>
                 </li>
         </div>
     </div>
